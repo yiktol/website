b397999930860e3cfcc573bcd8746285edf39060
yiktol <yik.tolentino@gmail.com>
2023-10-16 08:40:47 +0800

    Update

--- coffeeshop/get-cpu-load.php
+++ coffeeshop/get-cpu-load.php
@@ -1,12 +1,14 @@
 <?php
   # This code performs a simple vmstat and grabs the current CPU idle time
   $idleCpu = exec('vmstat 1 2 | awk \'{ for (i=1; i<=NF; i++) if ($i=="id") { getline; getline; print $i }}\'');
+  $uptime = exec('uptime -p');
 
   # Print out the idle time, subtracted from 100 to get the current CPU utilization
   $load = 100-$idleCpu;
   echo "<p>";
   echo "<table>";
   echo "<tr><td>Current CPU Load: $load %</td></tr>";
+  echo "<tr><td>Instance Uptime: $uptime</td></tr>";
   echo "</table>";
   echo "</p>";
   
